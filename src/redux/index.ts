9e3cf30f53bab13e7e89551a69e9ce8599d7e252
HusanboyevShodiyor <husanovshodiyor02@gmail.com>
2026-01-20 11:33:23 -0930

    udar

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,12 +1,25 @@
-import {configureStore} from "@reduxjs/toolkit";
-import  modalSlice from "./modal-store";
+// import {configureStore} from "@reduxjs/toolkit";
+// import  modalSlice from "./modal-store";
+
+// export const store = configureStore({
+//     reducer: {
+//         modalSlice,
+//     },
+// })
+
+
+// export type RootStore = ReturnType< typeof store.getState>;
+// export type DispatchType = typeof store.dispatch;
+
+import { configureStore } from "@reduxjs/toolkit";
+import modalSlice from "./modal-store";
 
 export const store = configureStore({
-    reducer: {
-        modalSlice,
-    },
-})
+  reducer: {
+    modalSlice,
+  },
+});
 
-
-export type RootStore = ReturnType< typeof store.getState>;
+// O'zgartirish shu yerda: RootStore -> RootState
+export type RootState = ReturnType<typeof store.getState>;
 export type DispatchType = typeof store.dispatch;
